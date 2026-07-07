c1ae98cf30157172664335f6cc48a8177098cd0e
erickChavesFiamoncini <erickechaves@gmail.com>
2026-07-07 14:28:56 -0300

    feat: criação de um endpoint para criar novas compras

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,6 +1,15 @@
 from .user import UserRegistrationSerializer, UserSerializer
 from .autor import AutorSerializer
 from .categoria import CategoriaSerializer
-from .compra import CompraSerializer
+from .compra import (
+    CompraSerializer,
+    CompraCreateUpdateSerializer, 
+    ItensCompraSerializer, 
+    ItensCompraCreateUpdateSerializer,
+)
 from .editora import EditoraSerializer
-from .livro import LivroListSerializer, LivroRetrieveSerializer, LivroSerializer
+from .livro import (
+    LivroListSerializer,
+    LivroRetrieveSerializer, 
+    LivroSerializer,
+)
